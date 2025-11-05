4923396f1875b7266ff34ab377b37a8505414e06
Thasmiga <vimalthasmiga1112@gmail.com>
2025-11-05 12:47:36 +0530

    basic route and server created

---
+++ app.js
@@ -0,0 +1,33 @@
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import weatherRoutes from "./routes/weatherRoutes.js";
+import clothesRoutes from "./routes/clothesRoutes.js";
+import occasionRoutes from "./routes/occasionRoutes .js"
+import partnerRoutes from "./routes/partnerRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import stylerRoutes from "./routes/stylerRoutes.js";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.get("/", (req, res) => res.send("API running"));
+
+app.use("/api/users", userRoutes);
+app.use("/api/weather", weatherRoutes);
+app.use("/api/partner", partnerRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/clothes", clothesRoutes);
+app.use("/api/styler", stylerRoutes);
+app.use("/api/occasion", occasionRoutes);
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
