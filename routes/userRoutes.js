a1479d36d1c27fae12b331f0a5c7361c023c82c3
Thasmiga <vimalthasmiga1112@gmail.com>
2025-11-08 07:07:34 +0530

    routes correction

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,17 @@
 import express from "express";
 import {
-  createClothes,
-  getAllClothes,
-  getClothesById,
-  updateClothes,
-  deleteClothes
-} from "../controllers/clothesController.js";
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/", createClothes);
-router.get("/", getAllClothes);
-router.get("/:id", getClothesById);
-router.put("/:id", updateClothes);
-router.delete("/:id", deleteClothes);
-
+router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 export default router;
